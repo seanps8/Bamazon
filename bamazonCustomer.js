e998ca850730072f04a3a6e044b9c09d17eb088d
Blazer <sstubbs012@gmail.com>
2018-10-24 19:40:10 -0400

    able to pull values from db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,4 +12,14 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    allProducts();
 })
+
+function allProducts () {
+    connection.query("SELECT * FROM products", function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+          }
+          console.log("-----------------------------------");
+    });
+};
