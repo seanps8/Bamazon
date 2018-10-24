cfec5a23ecad4afe9836bd318efdbbfe2455e019
Blazer <sstubbs012@gmail.com>
2018-10-24 19:33:22 -0400

    connected database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,14 @@
-console.log("working");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+})
